2e2af69140014708b8dc350c0b6364fea693d399
Siddhartha <sipurwar@cisco.com>
2023-03-16 13:15:44 +0530

    BUILD: Added more fields

--- Stat/stat.go
+++ Stat/stat.go
@@ -4,27 +4,16 @@ import (
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
 	"github.com/shirou/gopsutil/v3/host"
+	"github.com/shirou/gopsutil/v3/mem"
 )
 
-// type InfoStat struct {
-// 	Uptime               uint64 `json:"uptime"`
-// 	BootTime             uint64 `json:"bootTime"`
-// 	Procs                uint64 `json:"procs"`           // number of processes
-// 	OS                   string `json:"os"`              // ex: freebsd, linux
-// 	Platform             string `json:"platform"`        // ex: ubuntu, linuxmint
-// 	PlatformFamily       string `json:"platformFamily"`  // ex: debian, rhel
-// 	PlatformVersion      string `json:"platformVersion"` // version of the complete OS
-// 	KernelVersion        string `json:"kernelVersion"`   // version of the OS kernel (if available)
-// 	KernelArch           string `json:"kernelArch"`      // native cpu architecture queried at runtime, as returned by `uname -m` or empty string in case of error
-// 	VirtualizationSystem string `json:"virtualizationSystem"`
-// 	VirtualizationRole   string `json:"virtualizationRole"` // guest or host
-// 	HostID               string `json:"hostid"`             // ex: uuid
-// }
-
 type InfoStat struct {
-	uptime   bool
-	bootTime bool
-	procs    bool
+	uptime         bool
+	bootTime       bool
+	procs          bool
+	Free           bool
+	usedMemory     bool
+	usedPercentage bool
 }
 
 func (*InfoStat) SampleConfig() string {
@@ -32,17 +21,28 @@ func (*InfoStat) SampleConfig() string {
 }
 
 func (s *InfoStat) Gather(acc telegraf.Accumulator) error {
-	data, _ := host.Info()
+	host_data, _ := host.Info()
+	memory_data, _ := mem.VirtualMemory()
 
 	fields := make(map[string]interface{})
+
 	if s.uptime {
-		fields["up_time"] = data.Uptime
+		fields["up_time"] = host_data.Uptime
 	}
 	if s.procs {
-		fields["pros_nums"] = data.Procs
+		fields["pros_nums"] = host_data.Procs
 	}
 	if s.bootTime {
-		fields["boot_time"] = data.BootTime
+		fields["boot_time"] = host_data.BootTime
+	}
+	if s.Free {
+		fields["free_memory"] = memory_data.Free
+	}
+	if s.usedMemory {
+		fields["total_memory"] = memory_data.Total
+	}
+	if s.usedPercentage {
+		fields["used_percentage"] = memory_data.UsedPercent
 	}
 
 	tags := make(map[string]string)
@@ -53,5 +53,15 @@ func (s *InfoStat) Gather(acc telegraf.Accumulator) error {
 }
 
 func init() {
-	inputs.Add("processes_mes", func() telegraf.Input { return &InfoStat{procs: true, bootTime: true, uptime: true} })
+	inputs.Add("processes_mes", func() telegraf.Input {
+		return &InfoStat{
+			uptime:         true,
+			bootTime:       true,
+			procs:          true,
+			Free:           true,
+			usedMemory:     true,
+			usedPercentage: true,
+		}
+	})
 }
+
